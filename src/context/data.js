317b28ef754aff223709d486accb3a6962cbad93
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2024-07-04 00:01:45 +0530

    Configure server to serve React app

--- src/context/data.js
+++ src/context/data.js
@@ -1,27 +1,36 @@
-import React, { createContext, useState, useEffect,useContext } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+
+// Import API_BASE_URL from environment variables
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
     const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
+        setLoading(true);
         axios.get(`${API_BASE_URL}/api/data`)
-          .then(response => {
-            setData(response.data);
-          })
-          .catch(error => {
-            console.error('There was an error making the request!', error);
-          });
-      }, []);
+            .then(response => {
+                setData(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []); // Dependency array is empty to fetch data only once
 
     return (
-        <DataContext.Provider value={data}>
+        <DataContext.Provider value={{ data, loading, error }}>
             {children}
         </DataContext.Provider>
     );
 };
+
 const useData = () => useContext(DataContext);
 
 export { useData, DataProvider };
