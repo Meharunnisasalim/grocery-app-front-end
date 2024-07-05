0410292d5624fdb6e93fb7414cb3dceed8de3093
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2024-07-05 15:00:58 +0530

    Updated environment variables and server configuration

--- src/components/Form/SearchInput.js
+++ src/components/Form/SearchInput.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { useSearch } from '../../context/search';
 import { useData } from '../../context/data';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+useEffect(() => {
+    console.log('API_BASE_URL:', API_BASE_URL);
+  }, []);
+  
 const SearchInput = () => {
     const [values,setValues]=useSearch();
     const navigate=useNavigate();
